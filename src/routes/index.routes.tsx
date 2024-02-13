6576d765bf5a6ba5fdafc9cdc5a82573d078051c
RafaelTorresVIPe <Rafael.torres@somosvipe.com.br>
2024-02-13 20:00:18 -0300

    feat: adding navigation

---
+++ src/routes/index.routes.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const {Navigator, Screen} = createNativeStackNavigator();
+
+export const Routes = () => {
+  return (
+    <Navigator>
+      <Screen name="Home" component={HomeScreen} />
+    </Navigator>
+  );
+};
