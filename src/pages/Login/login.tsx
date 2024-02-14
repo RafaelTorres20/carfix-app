bbcefccd24a306355a002a5d376f8e609908d638
RafaelTorresVIPe <Rafael.torres@somosvipe.com.br>
2024-02-13 21:14:49 -0300

    feat: adding svg support

--- src/pages/Login/login.tsx
+++ src/pages/Login/login.tsx
@@ -1,9 +1,12 @@
+import React from 'react';
 import {View, Text, SafeAreaView} from 'react-native';
+import Logo from '../../assets/logo.svg';
 
 export const Login = () => {
   return (
     <SafeAreaView>
       <View>
+        <Logo width={120} height={40} fill={'#E74C3C'} />
         <Text>Login</Text>
       </View>
     </SafeAreaView>
--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {Forgot} from '../pages/Forgot';
 import {Login} from '../pages/Login';
